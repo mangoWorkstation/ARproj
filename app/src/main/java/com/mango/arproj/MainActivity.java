5654b5afa4ecafea62305778f69c10c04aa6ebe5
mango <ryanhowe@qq.com>
2018-05-30 09:21:05 +0800

    Vuforia installed.

--- app/src/main/java/com/mango/arproj/MainActivity.java
+++ app/src/main/java/com/mango/arproj/MainActivity.java
@@ -19,6 +19,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
