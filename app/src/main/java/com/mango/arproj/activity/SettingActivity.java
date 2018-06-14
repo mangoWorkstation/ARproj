adb187256610df2870671381d4776ab46b554b62
mango <ryanhowe@qq.com>
2018-06-14 09:25:30 +0800

    已完成退出登录的功能。

--- app/src/main/java/com/mango/arproj/activity/SettingActivity.java
+++ app/src/main/java/com/mango/arproj/activity/SettingActivity.java
@@ -1,11 +1,16 @@
 package com.mango.arproj.activity;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.mango.arproj.R;
+import com.mango.arproj.util.ARutil;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -19,5 +24,20 @@ public class SettingActivity extends AppCompatActivity {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(SettingActivity.this,android.R.layout.simple_list_item_1,titles);
         ListView listView = findViewById(R.id.listView_setting);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if(i==2){
+                    SharedPreferences.Editor editor = getSharedPreferences(ARutil.getSharePreferencePath(),MODE_PRIVATE).edit();
+                    editor.remove("token");
+                    editor.apply();
+                    Toast.makeText(SettingActivity.this,"您已退出登录",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            }
+        });
+
     }
+
 }
